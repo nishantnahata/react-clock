5708e85105eab843a1534ddb8a446286c6a2c432
Nishant Nahata <nishant.nahata97@gmail.com>
2018-08-29 12:22:48 +0530

    Fixed prop names.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import {observer} from 'mobx-react';
+import { observer } from 'mobx-react';
 import Clock from '../Clock';
 
 @observer
 export default class Home extends React.Component {
-
-    render() {
-        return <div>
-            <Clock clockColor='yellow' backgroundColor='black'/>
-        </div>;
-    }
+  render() {
+    return (
+      <div>
+        <Clock clockColor="black" backgroundColor="white" />
+      </div>
+    );
+  }
 }
